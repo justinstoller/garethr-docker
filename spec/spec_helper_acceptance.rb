747f3e5a9dd421cb7cc0afcb325f76323eadda33
Justin Stoller <justin.stoller@gmail.com>
2015-05-03 14:18:06 -0700

    update spec_helper_acceptance to use module version and possibly puppet-agent

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -3,12 +3,21 @@ require 'beaker-rspec/helpers/serverspec'
 require 'pry'
 
 unless ENV['BEAKER_provision'] == 'no'
-  hosts.each do |host|
-    # Install Puppet
-    if host.is_pe?
-      install_pe
+  if default.is_pe?
+    install_pe
+  else
+    if ENV['USE_PUPPET_AGENT']
+      install_options = {}
+      install_options[:version] = ENV['PUPPET_AGENT_VERSION'] if ENV['PUPPET_AGENT_VERSION']
+
+      install_puppet_agent(install_options)
     else
-      install_puppet
+      install_options                  = {:default_action => 'gem_install'}
+      install_options[:version]        = ENV['PUPPET_VERSION'] if ENV['PUPPET_VERSION']
+      install_options[:facter_version] = ENV['FACTER_VERSION'] if ENV['FACTER_VERSION']
+      install_options[:hiera_version]  = ENV['HIERA_VERSION']  if ENV['HIERA_VERSION']
+
+      install_puppet(install_options)
     end
   end
 end
@@ -23,7 +32,12 @@ RSpec.configure do |c|
   # Configure all nodes in nodeset
   c.before :suite do
     # Install module and dependencies
-    puppet_module_install(:source => proj_root, :module_name => 'docker')
+    install_options = { :source      => proj_root,
+                        :module_name => 'docker'   }
+
+    install_options[:version] = ENV['MODULE_VERSION'] if ENV['MODULE_VERSION']
+
+    puppet_module_install(install_options)
     hosts.each do |host|
       on host, puppet('module', 'install', 'puppetlabs-stdlib'), { :acceptable_exit_codes => [0,1] }
       on host, puppet('module', 'install', 'puppetlabs-apt', '--version', '1.8.0'), { :acceptable_exit_codes => [0,1] }
